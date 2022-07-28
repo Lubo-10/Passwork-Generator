e16849c99cce1302e7e0325fdca9a83a77c010be
Lubo-10 <bernlubo01@gmail.com>
2022-07-28 16:54:14 -0400

    PAssword-Generator

---
+++ README.md
@@ -0,0 +1,27 @@
+Password Generator
+
+Table of Contents
+
+About
+Built
+Installation
+Usage
+Credits
+License
+About The Project
+
+This is a password generator used to create random password.
+
+Built With
+
+HTML
+CSS
+JavaScript
+Installation
+
+
+Usage
+
+A application used to create a random password for your use.
+
+ScreenShot
---
+++ assets/javascript/test-script.js
@@ -0,0 +1,67 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+const myArrayUpper = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
+
+const myArrayLower = Array.from(Array(26)).map((e, i) => i + 97);
+const alphabetLower = myArrayLower.map((x) => String.fromCharCode(x));
+
+const arrayNumeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const arraySpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+function generatePassword() {
+  var results = "";
+  var numberOfCharacters = window.prompt("How many characters would you like your password to contain");
+  var characterQuantity = parseInt(numberOfCharacters);
+  if (characterQuantity >= 8 && characterQuantity <= 128) {
+    var lowerCase = window.confirm("click OK to confirm lowercase letter.");
+    var upperCase = window.confirm("Click OK to confirm uppercase letter.");
+    var numeric = window.confirm("Click OK to confirm numeric values");
+    var specialCharacters = window.confirm("Click OK to confirm special characters");
+  } else {
+    window.alert('That is an invalid entry. Type and entry between 8 and 128')
+    return generatePassword();
+  }
+  var okayButton = [];
+  if (upperCase == true) okayButton.push(alphabetUpper);
+  if (lowerCase == true) okayButton.push(alphabetLower);
+  if (numeric == true) okayButton.push(arrayNumeric);
+  if (specialCharacters == true) okayButton.push(arraySpecialCharacters);
+
+  if (upperCase || lowerCase || numeric || specialCharacters) {
+
+    for (var i = 0; i < characterQuantity; i++) {
+      var storeButton = Math.floor(Math.random() * okayButton.length);
+      var selectedArray = okayButton[storeButton];
+      results += selectedArray[Math.floor(Math.random() * selectedArray.length)];
+      // results += alphabetLower[Math.floor(Math.random() *26)];
+      // results += arrayNumeric[Math.floor(Math.random() *10)];
+      // results += arraySpecialCharacters[Math.floor(Math.random() *10)];
+      // else { window.alert('This is an invalid entry. Select an entry between 8 and 128');
+      // return generatePassword();
+    }
+  } else {
+    window.alert('This is an invalid entry. Select at least ONE to proceed');
+    return generateBtn();
+  }
+  return results;
+}
+
+// challenge make it so that if they hit cancel to many times instead of error have it prompt them to do it again
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
